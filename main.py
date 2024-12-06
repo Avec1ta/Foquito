818948fca173467a39b518d0876da4e089aea120
Avec1ta <espericuetaramirezanita4@gmail.com>
2024-12-06 16:07:58 -0500

    Update main.py
    2da interfaz

--- main.py
+++ main.py
@@ -1,65 +1,141 @@
-import streamlit as st
-import requests
 import os
+
+#import pyperclip
+import requests
+import streamlit as st
+
+# Configuración de la página
+st.set_page_config(page_title="Foquito", page_icon="💡", layout="wide")
+
+st.markdown(
+    """
+    <style>
+    .centered-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 3em;
+        font-weight: bold;
+        margin-bottom: 50px;
+    }
+    .custom-label {
+        font-size: 1.1em;
+        margin-bottom: 10px;
+    }
+    .right-column {
+        margin-left: 50px; 
+    }
+    .copy-button {
+        margin-left: 50px; 
+    }
+    .footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 50px;
+        font-size: 1.2em;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Mostrar el título centrado en la parte superior
+st.markdown('<div class="centered-title">Foquito💡</div>', unsafe_allow_html=True)
 
 dify_secret = os.environ['DIFY_API_KEY']
 
-st.image("foquito_logo.png", use_container_width=False, width=200)
-st.write("**Idea contenido en un ratito**")
+col1, col2 = st.columns([4, 5])
 
-st.markdown("""
-¡Hola! Soy un asistente que te ayudará generando ideas de contenido creativas para tus redes sociales, adaptadas a las necesidades de tu negocio y a tu público objetivo.
+with col1:
+  st.markdown('<div class="custom-label">Cuéntame de tu negocio aquí 👇</div>', unsafe_allow_html=True)
+  prompt = st.text_area(label="", \
+placeholder="Mi negocio es...", \
+height=150)
 
-Cuéntame: 
-- ¿A qué rubro perteneces?
-- ¿Qué productos o servicios ofreces? (si es un negocio) o ¿Sobre qué temas hablas? (si eres creador de contenido) 
-- ¿Cuál es la personalidad de tu marca? (formal, juvenil, alegre, etc.)
-- ¿Quiénes son tus clientes o público objetivo?
-- ¿Cuántas ideas necesitas? (máximo 20 por ahora 🙈)
-- ¿Para qué mes las necesitas?
-- ¿Hay fechas especiales que quieras considerar?
-""")
+  base_url = "https://api.dify.ai/v1"
+  path = "/workflows/run"
+  full_url = base_url + path
 
-prompt = st.text_area("¡Aquí te leo!👇:")
+  headers = {
+          "Authorization": f"Bearer {dify_secret}",
+          "Content-Type": "application/json"
+      }
 
-base_url = "https://api.dify.ai/v1"
-path = "/workflows/run"
-full_url = base_url + path
+  data = {
+          "inputs": {
+          "query": prompt
+        },
+        "response_mode": "blocking",
+        "user": "Emprendedor"
+    }
 
-headers = {
-    "Authorization": f"Bearer {dify_secret}",
-    "Content-Type": "application/json"
-}
+    # Variable para almacenar la respuesta del asistente
+  if 'assistant_response' not in st.session_state:
+        st.session_state.assistant_response = ""
 
-data = {
-    "inputs": {
-        "query": prompt
-    },
-    "response_mode": "blocking",
-    "user": "Emprendedor"
-}
-
-# Variable para almacenar la respuesta del asistente
-assistant_response = ""
-
-if st.button("Enviar"):
+# Botón de enviar justo debajo de la caja de texto
+  if st.button("Enviar"):
     if prompt:
-        # Enviar el prompt al workflow en Dify
-        response = requests.post(full_url, headers=headers, json=data)
-        response_json = response.json()
+        # Mostrar mensaje de cargando
+        with st.spinner("Cargando..."):
+            # Hacer la solicitud a la API de Dify
+            response = requests.post(full_url, headers=headers, json=data)
+            response_json = response.json()
 
-        # Verificar si la solicitud fue exitosa
-        if response.status_code == 200:
-            # Extraer la respuesta del asistente del campo "Propuestas"
-            assistant_response = response_json.get("data", {}).get("outputs", {}).get("Propuestas","No se pudo obtener la respuesta.")
-            
-        else:
-            assistant_response = f"Error: {response.status_code} - {response.text}"
+            if response.status_code == 200:
+                # Extraer la respuesta del asistente del campo "Propuestas"
+                st.session_state.assistant_response = response_json.get("data", {}).get("outputs", {}).get("Propuestas", "No se pudo obtener la respuesta.")
+            else:
+                st.session_state.assistant_response = f"Error: {response.status_code} - {response.text}"
     else:
-        st.write("Por favor, ingresa un prompt.")
+        st.warning("Por favor, escribe una solicitud.")
 
 
-# Mostrar la respuesta del asistente
-if assistant_response:
-    st.markdown(f"**Respuesta de Foquito 💡:**\n\n{assistant_response}")
-    st.image("recuerda_foquito.png", caption="", width=550)
+with col2:
+    if not st.session_state.assistant_response:
+        st.markdown('''
+        <div class="right-column">
+            <h3>Instrucciones</h3>
+            <p>Escribe en la caja de texto de la izquierda: el rubro de tu 
+            negocio, los productos o servicios que vendes, quiénes son tus 
+            clientes, cuál es la identidad de tu marca, cuántas ideas necesitas (máximo 
+            20 ideas por ahora)
+            y para qué mes las necesitas. Luego presiona 'Enviar' para obtener 
+            ideas de contenido para redes sociales. Un ejemplo de buen prompt 
+            sería:</p>
+            <pre>
+            Tengo un vivero, vendo plantas de interior y exterior, suculentas, 
+            macetas, abonos, fertilizantes, insecticidas orgánicos, etc. 
+            Mi marca se llama "Verdileza" y somos una marca ecológica, que ama 
+            la naturaleza y busca propagar este amor por las plantas y el 
+            equilibrio natural a todas las personas. Mis clientes suelen ser 
+            jóvenes, mayormente mujeres que les gusta la belleza que las 
+            plantas aportan a sus espacios. Deseo 7 ideas de contenido 
+            para setiembre.
+            </pre>
+        </div>
+        ''', 
+        unsafe_allow_html=True)
+    else:
+        st.markdown('''
+        <div class="right-column">
+            <h4>Respuesta:</h4>
+            <p>{}</p>
+        </div>
+        '''.format(st.session_state.assistant_response), unsafe_allow_html=True)
+        if len(st.session_state.assistant_response) > 450:
+            st.image("recuerda_foquito.png", caption="", width=550)
+        #st.markdown('<div class="copy-button">', unsafe_allow_html=True)
+        #if st.button("Copiar respuesta"):
+            # Use Streamlit's built-in clipboard functionality
+            #pyperclip.copy(st.session_state.assistant_response)
+            #st.success("Respuesta copiada al portapapeles")
+        #st.markdown('</div>', unsafe_allow_html=True)
+
+# Mensaje de texto con hipervínculo al final de la pantalla
+st.markdown('''
+<div class="footer">
+    <p>¿Tienes feedback? Envíalo por correo <a href="mailto:ana.vict.esp@gmail.com">aquí</a>.</p>
+</div>
+''', unsafe_allow_html=True)
